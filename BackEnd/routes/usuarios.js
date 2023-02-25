40c29a7a581cfa4f5db9e9195bd257136c01e524
OzPineapple <ozielcortespinna@gmail.com>
2023-02-25 05:41:42 -0600

    Cambiar los nombres a los usados en el driver

--- BackEnd/routes/usuarios.js
+++ BackEnd/routes/usuarios.js
@@ -4,13 +4,13 @@ const express = require('express');
 const db = require('../lib/mongodb.js');
 
 router.post('/login', (req, res) => {
-	if( ! db.user_exists(req.body.user) ){
+	if( ! db.admin_get(req.body.user) ){
 		res.status(404);
 		res.setHeader('Content-Type', 'text/plain');
 		res.write("No existe ese usuario");
 		res.send();
 		return;
-	} else if( ! db.user_login( req.body ) ) {
+	} else if( ! db.admin_login( req.body ) ) {
 		res.status(401);
 		res.setHeader('Content-Type', 'text/plain');
 		res.write("Credenciales erroneas");
