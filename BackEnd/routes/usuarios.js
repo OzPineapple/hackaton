d70bb986b396f78bc73d9d4035c5238e27df4321
OzPineapple <ozielcortespinna@gmail.com>
2023-02-25 11:54:51 -0600

    CRUD server interface ready for implementation

--- BackEnd/routes/usuarios.js
+++ BackEnd/routes/usuarios.js
@@ -6,7 +6,37 @@ const db = require('../lib/mongodb.js');
 router.post('/login', async (req, res) => {
 	try{
 		await db.admin_login( req.body );
+		req.session.usr = req.body.usr;
 		res.status(200).send();
+	}catch(e){
+		console.log(e);
+		res.status(e.status).send();
+	}
+});
+
+router.get('/logout', (req, res) => {
+	req.session.destroy();
+	res.status(200).send();
+});
+
+router.post('/edit', async (req, res) => {
+	try{
+		if( ! req.session.usr )
+			res.status(401).send();
+		await db.update_user( req.body );
+		res.status(201).send();
+	}catch(e){
+		console.log(e);
+		res.status(e.status).send();
+	}
+});
+
+router.post('/new', async (req, res) => {
+	try{
+		if( ! req.session.usr )
+			res.status(401).send();
+		await db.create_user( req.body );
+		res.status(201).send();
 	}catch(e){
 		console.log(e);
 		res.status(e.status).send();
