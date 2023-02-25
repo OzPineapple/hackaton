bebdff526cc6ff7e0728cb97f7214526f641eee0
HaruNoYukki <61102112+HaruNoYukki@users.noreply.github.com>
2023-02-25 05:23:14 -0600

    Cuentas
    Se han modulado las cuentas

--- BackEnd/Solana/Accounts.ts
+++ BackEnd/Solana/Accounts.ts
@@ -9,6 +9,8 @@ import {
     PublicKey,
   } from "@solana/web3.js";
   
+  import * as bs58 from "bs58";
+
   //Crea el sistema de cuentas
   (async () => {
     const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
@@ -48,84 +50,74 @@ import {
   })();
 
   //Calcula el costo de las cuentas
-
-(async () => {
-  const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
-
-  // length of data in the account to calculate rent for
-  const dataLength = 1500;
-  const rentExemptionAmount =
-    await connection.getMinimumBalanceForRentExemption(dataLength);
-  console.log({
-    rentExemptionAmount,
-  });
-})();
-
-//Crea cuentas
-import * as bs58 from "bs58";
-
-(async () => {
-  // connection
-  const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
-
-  // 5YNmS1R9nNSCDzb5a7mMJ1dwK9uHeAAF4CmPEwKgVWr8
-  const feePayer = Keypair.fromSecretKey(
-    bs58.decode(
-      "588FU4PktJWfGfxtzpAAXywSNt74AvtroVzGfKkVN1LwRuvHwKGr851uH8czM5qm4iqLbs1kKoMKtMJG4ATR7Ld2"
-    )
-  );
-
-  // G2FAbFQPFa5qKXCetoFZQEvF9BVvCKbvUZvodpVidnoY
-  const base = Keypair.fromSecretKey(
-    bs58.decode(
-      "4NMwxzmYj2uvHuq8xoqhY8RXg63KSVJM1DXkpbmkUY7YQWuoyQgFnnzn6yo3CMnqZasnNPNuAT2TLwQsCaKkUddp"
-    )
-  );
-
-  let basePubkey = base.publicKey;
-  let seed = "robot001";
-  let programId = SystemProgram.programId;
-
-  let derived = await PublicKey.createWithSeed(basePubkey, seed, programId);
-
-  const tx = new Transaction().add(
-    SystemProgram.createAccountWithSeed({
-      fromPubkey: feePayer.publicKey, // funder
-      newAccountPubkey: derived,
-      basePubkey: basePubkey,
-      seed: seed,
-      lamports: 1e8, // 0.1 SOL
-      space: 0,
-      programId: programId,
-    })
-  );
-
-  console.log(
-    `txhash: ${await sendAndConfirmTransaction(connection, tx, [
-      feePayer,
-      base,
-    ])}`
-  );
-})();
-
-//Transferencia
-
-(async () => {
-    // connection
+  export module CalCost{
+    export async function calCon() {
     const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
-  
-    // 5YNmS1R9nNSCDzb5a7mMJ1dwK9uHeAAF4CmPEwKgVWr8
+
+    // length of data in the account to calculate rent for
+    const dataLength = 1500;
+    const rentExemptionAmount =
+    await connection.getMinimumBalanceForRentExemption(dataLength);
+    console.log({
+      rentExemptionAmount,
+    });
+    }
+  }
+
+  //Creando una cuenta
+  export module CrearCuenta{
+    export async function crearCuenta(key){
+      // connection
+    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
     const feePayer = Keypair.fromSecretKey(
-      bs58.decode(
-        "588FU4PktJWfGfxtzpAAXywSNt74AvtroVzGfKkVN1LwRuvHwKGr851uH8czM5qm4iqLbs1kKoMKtMJG4ATR7Ld2"
-      )
+      bs58.decode(key)
     );
   
     // G2FAbFQPFa5qKXCetoFZQEvF9BVvCKbvUZvodpVidnoY
     const base = Keypair.fromSecretKey(
-      bs58.decode(
-        "4NMwxzmYj2uvHuq8xoqhY8RXg63KSVJM1DXkpbmkUY7YQWuoyQgFnnzn6yo3CMnqZasnNPNuAT2TLwQsCaKkUddp"
-      )
+      bs58.decode(key)
+    );
+    
+    let basePubkey = base.publicKey;
+    let seed = "robot001";
+    let programId = SystemProgram.programId;
+
+    let derived = await PublicKey.createWithSeed(basePubkey, seed, programId);
+
+    const tx = new Transaction().add(
+      SystemProgram.createAccountWithSeed({
+        fromPubkey: feePayer.publicKey, // funder
+        newAccountPubkey: derived,
+        basePubkey: basePubkey,
+        seed: seed,
+        lamports: 1e8, // 0.1 SOL
+        space: 0,
+        programId: programId,
+    })
+  );
+
+  console.log(
+      `txhash: ${await sendAndConfirmTransaction(connection, tx, [
+        feePayer,
+        base,
+      ])}`
+    );
+  }
+}
+
+//Transferencia
+export module HacerTrans{
+  export async function hacerTrans(key){
+    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+  
+    // 5YNmS1R9nNSCDzb5a7mMJ1dwK9uHeAAF4CmPEwKgVWr8
+    const feePayer = Keypair.fromSecretKey(
+      bs58.decode(key)
+    );
+  
+    // G2FAbFQPFa5qKXCetoFZQEvF9BVvCKbvUZvodpVidnoY
+    const base = Keypair.fromSecretKey(
+      bs58.decode(key)
     );
   
     let basePubkey = base.publicKey;
@@ -151,4 +143,5 @@ import * as bs58 from "bs58";
         base,
       ])}`
     );
-  })();
+  }
+}
