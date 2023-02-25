14c21049eed382341540b5359d3fae1ebc4cd136
OzPineapple <ozielcortespinna@gmail.com>
2023-02-25 16:40:40 -0600

    Bug sobreescribi la varaible, ya realiza su comportamiento esperado

--- BackEnd/routes/tienda.js
+++ BackEnd/routes/tienda.js
@@ -2,13 +2,9 @@ var router =  require('express').Router();
 
 const db = require('../lib/mongodb.js');
 
-const express = require('express');
-
 router.get('/explorar', async (req, res) => {
 	try{
-		const res = await db.event_getAll();
-		console.log( res );
-		res.status(200).send( res );
+		res.status(200).send((await db.event_getAll()).toArray());
 	}catch(e){
 		console.log(e);
 		res.status(e.status).send();
