0a0af248ad046e1725b75a906a1b480cfe7f8d7c
re1101 <romaro1101@gmail.com>
2023-02-25 12:30:20 -0600

    Funciones de la base y ediciones a ella

--- BackEnd/lib/mongodb.js
+++ BackEnd/lib/mongodb.js
@@ -114,11 +114,22 @@ driver.event_set = ({eventName, type, price, date, desc, org, ubi}) =>{
 		if (err)
 			throw(err)
 		else
-			console.log("Documento Insertado")
+			console.log("Documento Insertado");
 	})
 }
 
-driver.event_getAll = async eventoId => {
+driver.event_getByID = async eventId => {
+
+	const query = { id_text: eventId , fecha:{$gt: newDate().toISOString()} };
+	const options = {projection: {_id: 0}};
+	
+	const evento = await collEvento.findOne(query, options);
+
+	return evento.next();
+
+}
+
+driver.event_getAll = async () => {
 
 	const query = { fecha:{$gt: newDate().toISOString()} };
 	const options = {projection: {_id: 0}};
@@ -129,7 +140,7 @@ driver.event_getAll = async eventoId => {
 	
 }
 
-driver.usr_set = ({nom, correo, llavep, llavepr}) => {
+driver.usr_set = ({nom, correo, contra, llavep, llavepr}) => {
 	var size = 0;
 
 	collUsuario.countDocuments(function(err,num){
@@ -140,13 +151,90 @@ driver.usr_set = ({nom, correo, llavep, llavepr}) => {
 	})
 	size++;
 
-	var newUsuario = {id_text: size, mail: correo, name:nom, publicK: llavep, privateK: llavepr};
+	var newUsuario = {id_text: size, mail: correo, pass: contra, name:nom, publicK: llavep, privateK: llavepr};
 	collUsuario.insertOne(newUsuario, function(err, res){
 		if (err)
 			throw(err)
 		else
-			console.log("Usuario Registrado")
+			console.log("Usuario Registrado");
 	})
+}
+
+driver.usr_getByPrivateK = async priv => {
+	const query = { privateK: priv };
+	const options = {projection: {_id: 0}};
+	
+	const users = await collUsuario.find(query, options);
+	let count = await users.count();
+
+	if( count == 0 )
+		throw new CustomError( "UserNotFound", 404, 
+			"El usuario " + user + " no se encuentra en la base de datos " + process.env.npm_package_config_dbname
+		);
+	else if( count > 1 )
+		throw new CustomError( "Duplicated Record", 409,
+			"Demaciados usuarios duplicados, deberían ser únicos para la busqueda " + users
+		);
+	else if (count == 1)
+		return users.next();
+	else
+		throw new CustomError( "UnknownError", 500,
+			"Un error desconocido evita que el servidor pueda procesar la peticion"
+		);
+}
+
+driver.usr_getByMail = async corr => {
+	const query = { mail: corr };
+	const options = {projection: {_id: 0}};
+	
+	const users = await collUsuario.find(query, options);
+	let count = await users.count();
+
+	if( count == 0 )
+		throw new CustomError( "UserNotFound", 404, 
+			"El usuario " + user + " no se encuentra en la base de datos " + process.env.npm_package_config_dbname
+		);
+	else if( count > 1 )
+		throw new CustomError( "Duplicated Record", 409,
+			"Demaciados usuarios duplicados, deberían ser únicos para la busqueda " + users
+		);
+	else if (count == 1)
+		return users.next();
+	else
+		throw new CustomError( "UnknownError", 500,
+			"Un error desconocido evita que el servidor pueda procesar la peticion"
+		);
+}
+
+driver.set_ticket = ({idEvento, idUsr, token}) => {
+
+	var size = 0;
+
+	collBoleto.countDocuments(function(err,num){
+		if(err)
+			throw(err)
+		else
+			size = num;
+	})
+	size++;
+
+	var newBoleto = {id_text: size, evento: idEvento, nft: token, owner: idUsr, fecha: this.event_getByID.fecha};
+	collBoleto.insertOne(newBoleto, function(err,res){
+		if (err)
+			throw(err)
+		else
+			console.log("Boleto Generado");
+	})
+
+}
+
+driver.ticket_getByOwner = async idUsr => {
+	const query = { owner: idUSr, fecha:{$gt: newDate().toISOString()} };
+	const options = {projection: {_id: 0}};
+	
+	const bol = await collBoleto.find(query, options);
+	
+	return bol;
 }
 
 module.exports = driver;
