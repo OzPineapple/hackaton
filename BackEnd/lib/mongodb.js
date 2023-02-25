fa25cb747d7f0a1a0fe51021ebcf50076e19f912
OzPineapple <ozielcortespinna@gmail.com>
2023-02-25 07:25:43 -0600

    Cambiar a errores personalizados y tratar a los querys como colecciones

--- BackEnd/lib/mongodb.js
+++ BackEnd/lib/mongodb.js
@@ -18,22 +18,26 @@ driver.admin_get = async userAdmin => {
 	const query = { usr: userAdmin };
 	const options = {projection: {_id: 0}};
 	
-	const admin = await collAdmin.find(query, options);
-	if (admin.exists())
-		return admin;
-	else
-		return null;
+	const admins = await collAdmin.find(query, options);
+	let count = await admins.count();
+
+	if( count == 0 )
+		throw new CustomError( "UserNotFound", 404, 
+			"El usuario " + userAdmin + " no se encuentra en la base de datos " + process.env.npm_package_config_dbname
+		);
+	else if( count > 1 )
+		throw new CustomError( "InvalidDuplicate", 500,
+			"Demaciados usuarios duplicados, deberían ser únicos para la busqueda " + userAdmin
+		);
+	return admins.next();
 }
 
-driver.admin_login = ({ usr, pass }) => {
-	var val = false;
-
-	this.admin_get(usr, function(err, result){
-		if (err) throw (err);
-		if (pass == result.pass) val=true;
-	});
-	
-	return val;
+driver.admin_login = async ({ usr, pass }) => {
+	const admin = await driver.admin_get(usr);
+	if( admin.pass != pass )
+		throw new CustomError( "WrongPassword", 401,
+			"La contraseña no es correcta para el usuario " + usr 
+		);
 }
 
 driver.eventType_get = async eventType => {
