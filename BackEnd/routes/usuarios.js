1840c30125743b3ed3b59a7626128c5f3803dd6a
OzPineapple <ozielcortespinna@gmail.com>
2023-02-25 02:30:07 -0600

    Código funcional para el modulo de usuarios

--- BackEnd/routes/usuarios.js
+++ BackEnd/routes/usuarios.js
@@ -1,8 +1,24 @@
 var router =  require('express').Router();
 
 const express = require('express');
+const db = require('../lib/mongodb.js');
 
-router.post('/login', (req, res, next) => {
+router.post('/login', (req, res) => {
+	if( ! db.user_exists(req.body.user) ){
+		res.status(404);
+		res.setHeader('Content-Type', 'text/plain');
+		res.write("No existe ese usuario");
+		res.send();
+		return;
+	} else if( ! db.user_login( req.body ) ) {
+		res.status(401);
+		res.setHeader('Content-Type', 'text/plain');
+		res.write("Credenciales erroneas");
+		res.send();
+		return;
+	}
+	res.status(200);
+	res.send();
 });
 
 module.exports = router;
