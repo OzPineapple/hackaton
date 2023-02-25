1ebae4c31729a75226a3d8171b29d3bba1fca112
OzPineapple <ozielcortespinna@gmail.com>
2023-02-25 15:22:45 -0600

    Interfaces para la modificación y creación de usuarios administradores y clientes

--- BackEnd/routes/cliente.js
+++ BackEnd/routes/cliente.js
@@ -2,7 +2,26 @@ var router =  require('express').Router();
 
 const express = require('express');
 
-router.post('/login', (req, res, next) => {
+router.post('/edit', async (req, res) => {
+	try{
+		if( ! req.session.usr )
+			res.status(401).send();
+		await db.update_user( req.body );
+		res.status(201).send();
+	}catch(e){
+		console.log(e);
+		res.status(e.status).send();
+	}
+});
+
+router.post('/new', async (req, res) => {
+	try{
+		await db.create_user( req.body );
+		res.status(201).send();
+	}catch(e){
+		console.log(e);
+		res.status(e.status).send();
+	}
 });
 
 module.exports = router;
--- BackEnd/routes/usuarios.js
+++ BackEnd/routes/usuarios.js
@@ -30,28 +30,4 @@ router.get('/logout', (req, res) => {
 	res.status(200).send();
 });
 
-router.post('/edit', async (req, res) => {
-	try{
-		if( ! req.session.usr )
-			res.status(401).send();
-		await db.update_user( req.body );
-		res.status(201).send();
-	}catch(e){
-		console.log(e);
-		res.status(e.status).send();
-	}
-});
-
-router.post('/new', async (req, res) => {
-	try{
-		if( ! req.session.usr )
-			res.status(401).send();
-		await db.create_user( req.body );
-		res.status(201).send();
-	}catch(e){
-		console.log(e);
-		res.status(e.status).send();
-	}
-});
-
 module.exports = router;
