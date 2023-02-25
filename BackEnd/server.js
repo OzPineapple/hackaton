90f0477142351a19dee1e319d76b1f6750724f70
OzPineapple <ozielcortespinna@gmail.com>
2023-02-25 11:43:13 -0600

    Limpiar código no usado

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -6,7 +6,6 @@ const path = require('path');
 
 const app = express();
 const router = express.Router();
-//const db = require('./db');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname,"public")));
@@ -15,15 +14,12 @@ app.use(express.static(path.join(__dirname,"public")));
 const clientes_router		= require(path.join(__dirname,'routes','cliente'));
 const administrador_router	= require(path.join(__dirname,'routes','administrador'));
 const usuarios_router		= require(path.join(__dirname,'routes','usuarios'));
-//const vendedor_router		= require(path.join(__dirname,'routes','vendedor'));
 
 /* Configuraciones */
 /* Enrutadores */
 app.use("/clientes",		clientes_router		);
 app.use("/administrador",	administrador_router	);
 app.use("/usuarios",		usuarios_router	);
-// Dejaremos este modulo para el final si es que queda suficiente tiempo
-// app.use("/vendedor",		vendedor_router		);
 
 const port = process.env.npm_package_config_port || '8080';
 app.set('port', port);
