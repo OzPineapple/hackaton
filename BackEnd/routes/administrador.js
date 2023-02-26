97aa3d23e80ab9b0138ae1e7b986dc7ab5eb9b80
OzPineapple <ozielcortespinna@gmail.com>
2023-02-26 00:02:08 -0600

    CUD de eventos

--- BackEnd/routes/administrador.js
+++ BackEnd/routes/administrador.js
@@ -4,7 +4,7 @@ const express = require('express');
 
 router.post('/edit', async (req, res) => {
 	try{
-		await db.update_user( req.body );
+		await db.update_admin( req.body );
 		res.status(201).send();
 	}catch(e){
 		console.log(e);
@@ -16,7 +16,49 @@ router.post('/new', async (req, res) => {
 	try{
 		if( ! req.session.usr )
 			res.status(401).send();
-		await db.create_user( req.body );
+		await db.create_admin( req.body );
+		res.status(201).send();
+	}catch(e){
+		console.log(e);
+		res.status(e.status).send();
+	}
+});
+
+/* Las siguietes rutas estan pensadas para funcionar
+ * como un CRUD parcial sobre los eventos que serán
+ * introducidos por el administrador.
+ * Se enviar el req.body directo al controlador
+ * de mongodb.
+ */
+route.post('/evento/new', async (req, res) => {
+	try{
+		if( ! req.session.usr )
+			res.status(401).send();
+		await db.create_event( req.body );
+		res.status(201).send();
+	}catch(e){
+		console.log(e);
+		res.status(e.status).send();
+	}
+});
+
+route.post('/evento/edit', async (req, res) => {
+	try{
+		if( ! req.session.usr )
+			res.status(401).send();
+		await db.create_edit( req.body );
+		res.status(201).send();
+	}catch(e){
+		console.log(e);
+		res.status(e.status).send();
+	}
+});
+
+route.post('/evento/delete', async (req, res) => {
+	try{
+		if( ! req.session.usr )
+			res.status(401).send();
+		await db.create_delete( req.body );
 		res.status(201).send();
 	}catch(e){
 		console.log(e);
