54b068ce79e611797f50493478025c005a9ee3e8
AladinMont <gggamera@outlook.com>
2023-02-26 12:09:48 -0600

    Full back end casi finalisimo

--- BackEnd/routes/usuarios.js
+++ BackEnd/routes/usuarios.js
@@ -5,7 +5,27 @@ const db = require('../lib/mongodb.js');
 router.post('/login', async (req, res) => {
 	try{
 		req.session.usr = await db.admin_login( req.body );
+		console.log(req.session.usr);
 		res.status(200).send();
+		
+	}catch(e){
+		res.status(e.status).send();
+	}
+});
+
+router.post('/event', async (req, res) => {
+	try{
+		req.session.evento = await db.event_getByID( req.body );
+		res.status(200).send();
+		
+	}catch(e){
+		res.status(e.status).send();
+	}
+});
+
+router.get('/event/:id', async (req, res) => {
+	try{
+		res.status(200).send(await db.event_getByID( req.params.id ));
 	}catch(e){
 		res.status(e.status).send();
 	}
