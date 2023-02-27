0124cd46f2ed499200127c29d957f037865160cd
Jozelotl Joestar <ozielcortespinna@gmail.com>
2023-02-27 14:33:15 -0600

    Merge balance

--- BackEnd/Solana/Balance.js
+++ BackEnd/Solana/Balance.js
@@ -1,11 +1,5 @@
 const { clusterApiUrl, Connection, Keypair, PublicKey} = require("@solana/web3.js");
 
-<<<<<<< HEAD
-const connection = new Connection(clusterApiUrl('devnet'));
-
 module.exports = async (address) => {
-=======
-module.exports = async function getBalance(address) {
->>>>>>> 1e6f8e2d3c5e5c99befd29ebbdbff1ed50866446
     return await new Connection(clusterApiUrl('devnet')).getBalance(address);
 }
