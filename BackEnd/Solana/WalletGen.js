41cfb6ce617cebd682de3cced83e986b7ef0aa97
HaruNoYukki <61102112+HaruNoYukki@users.noreply.github.com>
2023-03-10 20:26:05 -0600

    Cambio API
    Se elimina la api antigua

--- BackEnd/Solana/WalletGen.js
+++
@@ -1,9 +0,0 @@
-const  { Keypair, PublicKey, SystemProgram }  = require( "@solana/web3.js");
-let bs58 = require("bs58");
-
-module.exports = () => {
-  const usrKpair = Keypair.generate();
-  const ursSK = usrKpair.secretKey;
-  const usrSk58 = bs58.encode(ursSK);
-  return usrSk58;
-}
--- BackEnd/Solana/solana.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	GeneraCuenta : require("./WalletGen"),
-	AddFondos : require("./PaySimulado/AñadirFondos"),
-	getBalance : require("./Balance.js").getBalance,
-	getPubkey : require("./Balance.js").getPubkey,
-	comprarBoleto : require("./PaySimulado/Transaccion.js"),
-	transferirBoleto : require("./NFT/Transfer.js")
-}
