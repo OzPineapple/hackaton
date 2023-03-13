854a2c2bd205830722a51ddf645ef639223f8681
Jozelotl Joestar <ozielcortespinna@gmail.com>
2023-03-12 21:58:48 -0600

    Client jwt validation

--- BackEnd/routes/client.js
+++ BackEnd/routes/client.js
@@ -7,10 +7,19 @@ var router = express.Router();
 
 router.use( async (req, res, next) => {
 	try{
-		const decode = await getJwt(req);
-		if( req.params.id != decode.id )
+		const decoded = getJwt( req );
+		debug( "usrT:" + decoded.usrT );
+		if( decoded.usrT != 2 )
 			return res.status(403).send();
+		debug("allowed");
+		next();
 	}catch(e){ switch(e.name){
+		case "undefined":
+			console.error(e);
+			return res.status(403).send();
+		case "NoBearer":
+			console.error(e);
+			return res.status(406).send();
 		default: next(e);
 	}}
 });
