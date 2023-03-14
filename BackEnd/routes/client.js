bea325e919ab8311e392ef56dadaeeee72d79bd6
Jozelotl Joestar <ozielcortespinna@gmail.com>
2023-03-13 23:29:40 -0600

    Debug for private K

--- BackEnd/routes/client.js
+++ BackEnd/routes/client.js
@@ -38,7 +38,9 @@ router.get('/', async (req, res, next) => {
 	try{
 		const decoded = getJwt( req );
 		var data = await db.getClient( decoded.id_text );
-		data.publicK = katamari.getPubKey( await db.getPrivateKeyOfClient( decoded.id_text ) );
+		const privateK = await db.getPrivateKeyOfClient( decoded.id_text );
+		debug('privateK: ' + typeof privateK );
+		data.publicK = await katamari.getPubKey( privateK );
 		debug('user asking for own info');
 		debug(data);
 		res.status(200);
