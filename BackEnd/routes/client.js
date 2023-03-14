fca1654cbe107001d2bc5eaa7b3e03d4e20f1a22
OzPineapple <ozielcortespinna@gmail.com>
2023-03-13 19:05:07 -0600

    cambiar console.log por debug en client

--- BackEnd/routes/client.js
+++ BackEnd/routes/client.js
@@ -22,10 +22,10 @@ router.use( async (req, res, next) => {
 		next();
 	}catch(e){ switch(e.name){
 		case "undefined":
-			console.error(e);
+			debug(e.name);
 			return res.status(403).send();
 		case "NoBearer":
-			console.error(e);
+			debug(e.name);
 			return res.status(406).send();
 		default: next(e);
 	}}
